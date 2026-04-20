76ddfed9009f8c3b6b23f21308b2c08375611443
Pietro Pires Bertato <166397026+cc24146@users.noreply.github.com>
2026-04-20 00:09:40 -0300

    struct analizador no .h, função de compilar programa e de bloco concluidas
    também foi adicionado os protótipos de função de cada um dos compila no .h

--- compilador/analex.h
+++ compilador/analex.h
@@ -16,8 +16,16 @@ typedef enum {
     enquanto,faca,vapara,mais,menos,vezes,dividir,
     igual,diferente,menor,menorouigual,maior,maiorouigual,
     e,ou,nao,abreparenteses,fechaparenteses,fechacolchetes,
-    abrecolchetes,virgula,pontoevirgula,doispontos,ponto,numero,identificador
+    abrecolchetes,virgula,pontoevirgula,doispontos,ponto,numero,identificador,fimdearquivo
 } token;
+
+typedef struct {    // struct para não ter que mandar tudo individualmente por parâmetros toda vez
+    FILE *arquivo;
+    char linha[MAX_COMPRIMENTO];
+    int pos;
+    char buf[MAX_COMPRIMENTO];
+    char palavraAtual[MAX_COMPRIMENTO]; 
+} Analisador;
 
 extern token tokens[];
 extern char *palavras[];
@@ -25,6 +33,16 @@ extern char *palavras[];
 token analex(char *palavra);
 int qualNumero(char *str);
 char *qualId(char *id);
-char *proxToken(char *src, int *pos, char *buf);
+char *separaToken(Analisador *an);
+void compila_programa(Analisador *an);  
+token proximoToken(Analisador *an);
+void compila_bloco(Analisador *an);
+void compila_parametros_formais(Analisador *an);
+void compila_comando(Analisador *an, token t);
+void compila_comando_sem_rotulo(Analisador *an);
+void compila_expressao(Analisador *an);
+void compila_expressao_simples(Analisador *an);
+void compila_termo(Analisador *an);
+void compila_fator(Analisador *an);
 
 #endif
--- compilador/main.c
+++ compilador/main.c
@@ -8,13 +8,13 @@
 #define MAX_COMPRIMENTO 256
 #define NUM_PALAVRAS 36
 
-token tokens[NUM_PALAVRAS + 2] = {
+token tokens[NUM_PALAVRAS + 3] = {
     programa,rotulo,tipo,variavel,procedimento,
     funcao,inicio,fim,atribuicao,se,entao,senao,
     enquanto,faca,vapara,mais,menos,vezes,dividir,
     igual,diferente,menor,menorouigual,maior,maiorouigual,
     e,ou,nao,abreparenteses,fechaparenteses,fechacolchetes,
-    abrecolchetes,virgula,pontoevirgula,doispontos,ponto,numero,identificador
+    abrecolchetes,virgula,pontoevirgula,doispontos,ponto,numero,identificador,fimdearquivo
 };
 
 char *palavras[NUM_PALAVRAS] = {
@@ -26,79 +26,93 @@ char *palavras[NUM_PALAVRAS] = {
     "]","[",",",";",":","."
 };
 
-int main(){
-    FILE *file;
-    file = fopen("exemplo.txt", "r");
-    if (file == NULL){
+int main() {
+    Analisador an;
+    an.pos = 0;
+    an.linha[0] = '\0';
+    an.palavraAtual[0] = '\0';
+
+    an.arquivo = fopen("exemplo.txt", "r");
+    if (an.arquivo == NULL) {
         printf("Erro ao abrir o arquivo\n");
         return 1;
     }
 
-    char linha[MAX_COMPRIMENTO];
-    int numLinha = 0;
+    // Carrega a primeira linha antes de começar
+    fgets(an.linha, MAX_COMPRIMENTO, an.arquivo);
 
-    while (fgets(linha, MAX_COMPRIMENTO, file) != NULL) {
-    int pos = 0;
-    char buf[MAX_COMPRIMENTO];
-    char *palavra;
+    // Dispara o analisador sintático
+    compila_programa(&an);
 
-        while ((palavra = proxToken(linha, &pos, buf)) != NULL) {
-            token t = analex(palavra);
-            if (t == numero) {
-                int val = qualNumero(palavra);
-            } else if (t == identificador) {
-                char *id = qualId(palavra);
-            } else {
-                // palavra reservada ou símbolo
-            }
-        }
-    }
-
-    fclose(file);
+    fclose(an.arquivo);
     return 0;
 }
 
-
-// src é a linha inteira
+// linha é a linha inteira
 // pos é o índice do caractere atual da linha
 // buf é o buffer onde o token será excrito
+// as setinhas são ponteiros usados para acessar cada membro da struct já que ela foi passada como ponteiro no parâmetro
 
-char *proxToken(char *src, int *pos, char *buf){
+char *separaToken(Analisador *an) {
+    while (an->linha[an->pos] && isspace((unsigned char)an->linha[an->pos]))    // pula espaços até acahr qualquer caractere
+        an->pos++;
 
-    while (src[*pos] && isspace(src[*pos])) // pula espaços
-        (*pos)++;
-    
-    if(!src[*pos])
+    if (!an->linha[an->pos])                                                    // se não tiver caracteres e só tiver espaços, retorna null
         return NULL;
-    
-    int start = *pos;   // guarda onde o token começa 
-    int i = 0;          // índice de escrita no buf
-    char c = src[*pos]; // caractere atual
-    if (isdigit((unsigned char)c)) {
-    while (isdigit((unsigned char)src[*pos])) buf[i++] = src[(*pos)++];
+
+    int i = 0;  // índice de escrita no buf
+    char c = an->linha[an->pos];    // caractere atual
+
+    if (isdigit((unsigned char)c)) {                        // se deixar só c por algum motivo não reconhece os números
+        while (isdigit((unsigned char)an->linha[an->pos]))
+            an->buf[i++] = an->linha[an->pos++];            // pega todos números um do lado do outro
+    }
+    else if (isalpha((unsigned char)c) || c == '_') {       // se começa com letra ou com underline lê até não ser nenhum dos dois ou algum número
+        while (isalnum((unsigned char)an->linha[an->pos]) || an->linha[an->pos] == '_')
+            an->buf[i++] = an->linha[an->pos++];
+    }
+    else if (                                           // verifica se é literalmente qualquer operador lógico de dois caracteres em c (pelo menso eu acho que coloquei todos)
+        (c == ':' && an->linha[an->pos+1] == '=') ||
+        (c == '<' && an->linha[an->pos+1] == '>') ||
+        (c == '<' && an->linha[an->pos+1] == '=') ||
+        (c == '>' && an->linha[an->pos+1] == '=') ||
+        (c == '=' && an->linha[an->pos+1] == '=') ||
+        (c == '!' && an->linha[an->pos+1] == '=') ||
+        (c == '&' && an->linha[an->pos+1] == '&') ||
+        (c == '|' && an->linha[an->pos+1] == '|') ||
+        (c == '+' && an->linha[an->pos+1] == '+') ||
+        (c == '-' && an->linha[an->pos+1] == '-') ||
+        (c == '+' && an->linha[an->pos+1] == '=') ||
+        (c == '-' && an->linha[an->pos+1] == '=') ||
+        (c == '*' && an->linha[an->pos+1] == '=') ||
+        (c == '/' && an->linha[an->pos+1] == '=') ||
+        (c == '%' && an->linha[an->pos+1] == '=') ||
+        (c == '<' && an->linha[an->pos+1] == '<') ||
+        (c == '>' && an->linha[an->pos+1] == '>') ||
+        (c == '-' && an->linha[an->pos+1] == '>')
+    ) {
+        an->buf[i++] = an->linha[an->pos++];                // se for avaça duas posições
+        an->buf[i++] = an->linha[an->pos++];
+    }
+    else {                                                  // só sobra token de 1 caractere
+        an->buf[i++] = an->linha[an->pos++];
     }
 
-    else if (isalpha(c) || c == '_') {
-        while (isalnum(src[*pos]) || src[*pos] == '_'){
-            buf[i++] = src[(*pos)++];
-        }
+    an->buf[i] = '\0';                                      // fecha a string para não dar problema depois 
+    return an->buf;                                         // retorna só uma "palavra"
+}
+
+token proximoToken(Analisador *an) {
+    char *palavra;
+
+    while ((palavra = separaToken(an)) == NULL) {
+        if (fgets(an->linha, MAX_COMPRIMENTO, an->arquivo) == NULL)
+            return fimdearquivo;
+        an->pos = 0;
     }
 
-    else if ((c == ':' && src[*pos+1] == '=') ||
-            (c == '<' && src[*pos+1] == '>') ||
-            (c == '<' && src[*pos+1] == '=') ||
-            (c == '>' && src[*pos+1] == '=')) {
-        buf[i++] = src[(*pos)++];  // lê 1º caractere
-        buf[i++] = src[(*pos)++];  // lê 2º caractere
-    }
-
-
-    else {
-        buf[i++] = src[(*pos)++];
-    }
-
-    buf[i] = '\0';
-    return buf;
+    strcpy(an->palavraAtual, palavra);
+    return analex(palavra);
 }
 
 token analex(char *palavra) {
@@ -127,4 +141,220 @@ char *qualId(char *id) {
 }
 
 
+void compila_programa(Analisador *an) {
+    token t = proximoToken(an);
+    if (t != programa) {
+        printf("Esperava-se a palavra PROGRAM\n");
+        exit(1);
+    }
+
+    t = proximoToken(an);
+    if (t != identificador) {
+        printf("Esperava-se um identificador após PROGRAM\n");
+        exit(1);
+    }
+
+    t = proximoToken(an);
+    if (t != abreparenteses) {
+        printf("Esperava-se '('\n");
+        exit(1);
+    }
+
+    do {
+        t = proximoToken(an);
+        if (t != identificador) {
+            printf("Esperava-se um identificador\n");
+            exit(1);
+        }
+        t = proximoToken(an);
+        if (t != virgula && t != fechaparenteses) {
+            printf("Esperava-se ',' ou ')'\n");
+            exit(1);
+        }
+    } while (t == virgula);
+
+    t = proximoToken(an);
+    if (t != pontoevirgula) {
+        printf("Esperava-se ';'\n");
+        exit(1);
+    }
+
+    compila_bloco(an);
+
+    t = proximoToken(an);
+    if (t != ponto) {
+        printf("Esperava-se '.'\n");
+        exit(1);
+    }
+
+    // Verifica fim de arquivo após o ponto final
+    t = proximoToken(an);
+    if (t != fimdearquivo) {
+        printf("Esperava-se fim de arquivo após '.'\n");
+        exit(1);
+    }
+
+    printf("Programa sintaticamente correto!\n");
+}
+void compila_bloco(Analisador *an) {
+    token t = proximoToken(an);
+    if (t == rotulo){
+        t = proximoToken(an);
+        if(t != numero){
+            printf("Esperava-se um número após 'label'!\n");
+            exit(1);
+        }
+        t = proximoToken(an);
+        while (t == virgula){
+            t = proximoToken(an);
+            if (t != numero){
+                printf("Esperava-se um número após ','!\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+        }
+
+        if(t != pontoevirgula){
+            printf("Esperava-se um ';'!\n");
+            exit(1);
+        }
+        t = proximoToken(an);
+    }
+
+    if (t == tipo){
+        do{
+            t = proximoToken(an);
+    
+            if ( t != identificador){
+                printf("Esperava-se um identificador após 'type'!\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+            if ( t != igual){
+                printf("Esperava-se um '=' após um identificador!\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+            if ( t != identificador){
+                printf("Esperava-se um tipo!\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+            if (t != pontoevirgula) {
+                printf("Esperava-se ';'\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+        }while(t == identificador);
+    }
+
+    if (t == variavel){
+        do{
+            t = proximoToken(an);
+            if(t != identificador){
+                printf("Esperava-se um identificador após 'var'!\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+            while (t == virgula){
+                t = proximoToken(an);
+                if (t != identificador){
+                    printf("Esperava-se um identificador após ','!\n");
+                    exit(1);
+                }
+                t = proximoToken(an);
+            }
+            if(t != doispontos){
+                printf("Esperava-se ':'!\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+            if ( t != identificador){
+                printf("Esperava-se um tipo!\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+            if (t != pontoevirgula) {
+                printf("Esperava-se ';'\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+
+        }while(t == identificador);
+    }
+
+    while (t == procedimento || t == funcao) {
+        if (t == procedimento){
+            t = proximoToken(an);
+            if(t != identificador){
+                printf("Esperava-se um identificador após 'procedure'!\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+            if(t == abreparenteses){
+                compila_parametros_formais(an);
+                t = proximoToken(an);
+            }
+            if (t != pontoevirgula) {
+                printf("Esperava-se ';' após 'procedure'!\n");
+                exit(1);
+            }
+            compila_bloco(an);
+            t = proximoToken(an);
+            if (t != pontoevirgula) {
+                printf("Esperava-se ';' após bloco do 'procedure'!\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+        } else {
+            t = proximoToken(an);
+            if(t != identificador){
+                printf("Esperava-se um identificador após 'function'!\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+            if(t == abreparenteses){
+                compila_parametros_formais(an);
+                t = proximoToken(an);
+            }
+            if (t != doispontos) {
+                printf("Esperava-se ':' após 'function'!\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+            if(t != identificador){
+                printf("Esperava-se um identificador após ':'!\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+            if (t != pontoevirgula) {
+                printf("Esperava-se ';' após 'function'!\n");
+                exit(1);
+            }
+            compila_bloco(an);
+            t = proximoToken(an);
+            if (t != pontoevirgula) {
+                printf("Esperava-se ';' após bloco do 'function'!\n");
+                exit(1);
+            }
+            t = proximoToken(an);
+        }
+    }
+
+    if (t != inicio) {
+    printf("Esperava-se 'begin'\n");
+    exit(1);
+    }
+    t = proximoToken(an);
+    while (t != fim) {
+        if (t == fimdearquivo) {
+            printf("Esperava-se 'end'\n");
+            exit(1);
+        }
+        compila_comando(an, &t);
+        if (t == pontoevirgula) {
+            t = proximoToken(an);
+        }
+    }
+}
 
